d3e73d818497d16f8a4e4cf07b9d36ecd35047b9
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 09:21:11 -0500

    Update main.tf
    updated subnets prefixes for new azureRM version

--- main.tf
+++ main.tf
@@ -328,12 +328,12 @@ resource "azurerm_virtual_network" "spoke1-vnet" {
   name                = "spoke1-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.150.0.0/24"
+    address_prefixes     = ["10.150.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.150.1.0/24"
+    address_prefixes     = ["10.150.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -350,12 +350,12 @@ resource "azurerm_virtual_network" "spoke2-vnet" {
   name                = "spoke2-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.250.0.0/24"
+    address_prefixes     = ["10.250.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.250.1.0/24"
+    address_prefixes     = ["10.250.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -373,12 +373,12 @@ resource "azurerm_virtual_network" "spoke3-vnet" {
   name                = "spoke3-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.50.0.0/24"
+    address_prefixes     = ["10.50.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.50.1.0/24"
+    address_prefixes     = ["10.50.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -396,12 +396,12 @@ resource "azurerm_virtual_network" "onprem-vnet" {
   name                = "onprem-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "192.168.0.0/24"
+    address_prefixes     = ["192.168.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "192.168.1.0/24"
+    address_prefixes     = ["192.168.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -419,12 +419,12 @@ resource "azurerm_virtual_network" "onprem2-vnet" {
   name                = "onprem2-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "172.16.0.0/24"
+    address_prefixes     = ["172.16.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "172.16.1.0/24"
+    address_prefixes     = ["172.16.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -902,8 +902,7 @@ resource "azurerm_virtual_network_gateway_connection" "to-azure2" {
 resource "azurerm_route_table" "RT" {
   name                          = "to-home"
   location                      = azurerm_resource_group.RG.location
-  resource_group_name           = azurerm_resource_group.RG.name
-  disable_bgp_route_propagation = false
+  resource_group_name           = azurerm_resource_group.RG.name  
 
   route {
     name           = "tohome"
